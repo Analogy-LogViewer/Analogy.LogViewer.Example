0023bb2904c824c133708303e90e77a831104aad
Lior Banai <liorbanai@gmail.com>
2020-03-03 12:51:25 +0200

    update interface

--- Analogy.LogViewer.Example/OfflineExampleDataProvider.cs
+++ Analogy.LogViewer.Example/OfflineExampleDataProvider.cs
@@ -1,23 +1,22 @@
-﻿using System;
+﻿using Analogy.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Analogy.Interfaces;
 
 namespace Analogy.LogViewer.Example
 {
     class OfflineExampleDataProvider : IAnalogyOfflineDataProvider
     {
-
+        public bool DisableFilePoolingOption { get; } = false;
         public Guid ID { get; }
         public string OptionalTitle { get; }
 
         public bool CanSaveToLogFile { get; } = false;
         public string FileOpenDialogFilters { get; } = "All supported Analogy log file types|*.log;*.json|Plain Analogy XML log file (*.log)|*.log|Analogy JSON file (*.json)|*.json";
-        public string FileSaveDialogFilters { get; }=String.Empty;
+        public string FileSaveDialogFilters { get; } = String.Empty;
         public IEnumerable<string> SupportFormats { get; } = new[] { "*.log", "*.json" };
         public string InitialFolderFullPath { get; } = Environment.CurrentDirectory;
 
@@ -44,12 +43,12 @@ namespace Analogy.LogViewer.Example
 
         public IEnumerable<FileInfo> GetSupportedFiles(DirectoryInfo dirInfo, bool recursiveLoad)
         {
-           return new List<FileInfo>(0);
+            return new List<FileInfo>(0);
         }
 
         public Task SaveAsync(List<AnalogyLogMessage> messages, string fileName)
         {
-           return Task.CompletedTask;
+            return Task.CompletedTask;
         }
 
         public bool CanOpenFile(string fileName)
@@ -61,6 +60,7 @@ namespace Analogy.LogViewer.Example
         {
             return fileNames.All(CanOpenFile);
         }
+
 
     }
 }
