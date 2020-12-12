1371c021e775adead7484c95806aac03ea2b1808
Lior Banai <liorbanai@gmail.com>
2020-12-12 14:46:36 +0200

    add policy enforce example

---
+++ Analogy.LogViewer.Example/IAnalogy/ExamplePolicyEnforcer.cs
@@ -0,0 +1,7 @@
+﻿namespace Analogy.LogViewer.Example.IAnalogy
+{
+    public class ExamplePolicyEnforcer : Template.AnalogyPolicyEnforcer
+    {
+        public override bool DisableUpdates { get; set; } = false;
+    }
+}
