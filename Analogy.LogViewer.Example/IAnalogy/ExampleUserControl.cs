93e35f169036ba7ba28c902cebba744982e42b54
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2021-03-26 12:40:52 +0300

    add Custom User Control example

---
+++ Analogy.LogViewer.Example/IAnalogy/ExampleUserControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Analogy.Interfaces;
+using Analogy.Interfaces.DataTypes;
+
+namespace Analogy.LogViewer.Example.IAnalogy
+{
+    public class ExampleUserControl :IAnalogyCustomUserControl
+    {
+        public Task InitializeUserControl(Control hostingControl, IAnalogyLogger logger)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task UserControlRemoved()
+        {
+            return Task.CompletedTask;
+        }
+
+        public UserControl UserControl { get; } = new ExampleUserControlUC();
+        public Guid Id { get; set; } = new Guid("ec1264aa-d503-4888-9772-572faa3f9a0c");
+        public Image? SmallImage { get; set; }
+        public Image? LargeImage { get; set; }
+        public string Title { get; set; } = "Example User Control";
+        public AnalogyToolTip? ToolTip { get; set; }
+    }
+}
---
+++ Analogy.LogViewer.Example/IAnalogy/ExampleUserControlUC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analogy.LogViewer.Example.IAnalogy
+{
+    public partial class ExampleUserControlUC : UserControl
+    {
+        public ExampleUserControlUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
